c3aeb0cc21ffa5ce47e54c57d5aeb95f46647c1d
Rufo Sanchez <rufo@rufosanchez.com>
2011-09-08 18:16:06 -0400

    added description to project

--- lib/fb_graph/project.rb
+++ lib/fb_graph/project.rb
@@ -1,4 +1,11 @@
 module FbGraph
   class Project < Klass
+    attr_reader :description
+
+    def initialize(identifier, attributes = {})
+      super
+
+      @description = attributes[:description]
+    end
   end
 end
